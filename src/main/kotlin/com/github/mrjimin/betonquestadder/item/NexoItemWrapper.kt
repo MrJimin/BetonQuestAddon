aac1e8b28fbfd008e281cd3dab5137e696b395ae
MrJimin <aa090402@naver.com>
2025-07-08 05:19:04 +0900

    init

--- src/main/kotlin/com/github/mrjimin/betonquestadder/item/NexoItemWrapper.kt
+++ src/main/kotlin/com/github/mrjimin/betonquestadder/item/NexoItemWrapper.kt
@@ -1,11 +1,9 @@
 package com.github.mrjimin.betonquestadder.item
 
-import com.nexomc.nexo.api.NexoItems
 import org.betonquest.betonquest.api.profile.Profile
 import org.betonquest.betonquest.instruction.variable.Variable
 import org.betonquest.betonquest.item.QuestItem
 import org.betonquest.betonquest.item.QuestItemWrapper
-import org.bukkit.Bukkit
 
 class NexoItemWrapper(
     private val itemId: Variable<String>
@@ -13,16 +11,6 @@ class NexoItemWrapper(
 
     override fun getItem(profile: Profile?): QuestItem? {
         val id = itemId.getValue(profile)
-        if (id == null) {
-            Bukkit.getLogger().warning("[NexoItemWrapper] Failed to resolve itemId from variable.")
-            return null
-        }
-
-        val exists = NexoItems.exists(id)
-        if (!exists) {
-            Bukkit.getLogger().warning("[NexoItemWrapper] Item ID '$id' not registered in NexoItems.")
-        }
-
         return NexoItem(id)
     }
 }
